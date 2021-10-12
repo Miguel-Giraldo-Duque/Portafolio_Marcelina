2164eee4134001586459d8087dd033236d5d7d52
Duke984 <miguelgiradu@gmail.com>
2021-10-12 15:32:53 -0500

    Creacion y finalizacion de seccion certificados

--- index.js
+++ index.js
@@ -46,3 +46,22 @@ function toggleSkills() {
 skillsHeader.forEach((elementos)=> {
     elementos.addEventListener("click",toggleSkills)
 });
+
+const tabs = document.querySelectorAll("[data-target]"),
+    tabContens = document.querySelectorAll("[data-content]")
+
+tabs.forEach(tab =>{
+    tab.addEventListener("click", () =>{
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContens.forEach(tabcontent =>{
+            tabcontent.classList.remove("qualification__active")
+        })
+        target.classList.add("qualification__active")
+
+        tab.forEach(tab =>{
+            tab.classList.remove("qualification__active")
+        })
+        tab.classList.add("qualification__active")
+    })
+})
